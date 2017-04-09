62ac70c61405576cebf3bea2c35402ec68fa4f72
Eddie Antonio Santos <easantos@ualberta.ca>
2017-04-08 20:19:19 -0600

    got it working.

--- js_tokenize.py
+++ js_tokenize.py
@@ -8,8 +8,12 @@ import signal
 import socket
 import struct
 import tempfile
+import logging
 
 from pathlib import Path
+from typing import Any, Sequence
+
+from sensibility import Token  # type: ignore
 
 THIS_DIRECTORY = Path(__file__).parent
 TOKENIZE_JS_BIN = THIS_DIRECTORY / 'tokenize-js' / 'wrapper.sh'
@@ -29,22 +33,19 @@ class Server:
         os.kill(self._pid, signal.SIGTERM)
 
     def check_syntax(self, contents: str) -> bool:
-        value = self._communicate('?', contents.encode('UTF-8'))
-        if value == 'true':
-            return True
-        elif value == 'false':
-            return False
-        else:
-            assert False, f"Unknown value: {value!r}"
+        return self._communicate('?', contents.encode('UTF-8'))
 
-    def tokenize(self, contents: str) -> str:
-        ...
+    def tokenize(self, contents: str) -> Sequence[Token]:
+        value = self._communicate('T', contents.encode('UTF-8'))
+        if isinstance(value, list):
+            return [Token.from_json(t) for t in value]
+        raise Exception(f"Bad return: {value!r}")
 
     def close(self) -> None:
         self.quit()
         self.server_address.unlink()
 
-    def _communicate(self, code: str, payload: bytes) -> str:
+    def _communicate(self, code: str, payload: bytes) -> Any:
         # Establish a new socket.
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.connect(str(self.server_address))
@@ -55,9 +56,29 @@ class Server:
         ]
         for part in parts:
             sock.send(part)
-        msg = sock.recv(4096)
+        msg = self._slurp(sock)
         sock.close()
-        return msg.decode('utf-8')
+        return json.loads(msg.decode('UTF-8'))
+
+    def _slurp(self, sock: socket.socket) -> bytes:
+        # Wait for the first few messages
+        sock.settimeout(30)
+        chunks = [sock.recv(128)]
+        logging.debug(f'initial read: {len(chunks[0])}')
+        sock.setblocking(False)
+        while True:
+            try:
+                chunk = sock.recv(4096)
+            except BlockingIOError:
+                break
+            logging.debug(f'subsequent read: {len(chunk)}')
+            if len(chunk) == 0:
+                break
+            chunks.append(chunk)
+
+        buf = b''.join(chunks)
+        logging.debug(f'read {len(buf)} bytes total')
+        return buf
 
     def _spawn(self) -> int:
         pid = os.fork()
@@ -74,11 +95,13 @@ _server.connect()
 atexit.register(_server.close)
 
 check_syntax = _server.check_syntax
+tokenize = _server.tokenize
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     try:
         while True:
             code = input('> ')
-            print(check_syntax(code))
+            print(tokenize(code))
     except EOFError:
         pass
