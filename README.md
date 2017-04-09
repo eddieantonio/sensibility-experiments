2a53d2cccd9c78544ef513ddb1c95a7a5d8ea258
Eddie Antonio Santos <easantos@ualberta.ca>
2017-04-09 09:51:18 -0600

    Add license and README.

---
+++ README.md
@@ -0,0 +1,8 @@
+Sensibility experiments
+=======================
+
+Trying to cluster based on # of tokens / # of lines.
+
+    Split at 85.8
+      last handwritten: 85.6: shandong.js
+      first generated:  86.0: Main.js
--- tokenize-js/index.js
+++ tokenize-js/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
- * Copyright 2016 Eddie Antonio Santos <easantos@ualberta.ca>
+ * Copyright 2016, 2017 Eddie Antonio Santos <easantos@ualberta.ca>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
